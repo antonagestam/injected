74a76f91055f8e0cfd0ad901de5b588cdfc6e75b
Anton Agestam <git@antonagestam.se>
2022-09-18 00:35:04 +0200

    Bump version (#3)

--- src/injected/__init__.py
+++ src/injected/__init__.py
@@ -2,5 +2,5 @@ from ._base import depends
 from ._base import resolver
 from ._base import seed_context
 
-__version__ = "0.1.0"
+__version__ = "0.1.1"
 __all__ = ("depends", "resolver", "seed_context")
