bbed2571ac39e840b4e892f7091e3784065b4220
Anton Agestam <git@antonagestam.se>
2025-05-10 23:34:14 +0200

    fix: Remove obsolete static version from __init__

--- src/injected/__init__.py
+++ src/injected/__init__.py
@@ -1,6 +1,13 @@
 from ._base import depends
 from ._base import resolver
 from ._base import seed_context
+from ._version import __version__
+from ._version import __version_tuple__
 
-__version__ = "0.1.1"
-__all__ = ("depends", "resolver", "seed_context")
+__all__ = (
+    "__version__",
+    "__version_tuple__",
+    "depends",
+    "resolver",
+    "seed_context",
+)
